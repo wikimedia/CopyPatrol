079f529add022cd948fc042e874ec683878bfbae
Niharika Kohli <niharikakohli29@gmail.com>
2016-04-02 14:59:43 +0300

    Fix credentials

--- public_html/index.php
+++ public_html/index.php
@@ -8,10 +8,17 @@ echo 'Hello, I\'m plagiabot!';
 
 $query = "SELECT id, project, page_title, report FROM copyright_diffs LIMIT 10";
 
+echo $query;
+
 $result = mysqli_query( $link, $query );
+
+var_dump( $result );
 
 if ( $result->num_rows > 0 ) {
 	while ( $row = $result->fetch_assoc() ) {
 		echo $row['id'], $row['project'], $row['page_title'], $row['report'];
 	}
 }
+
+
+?>
