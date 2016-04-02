bb4e3afa445f40d3a1ab65988f3e9da0e92adaf7
Niharika Kohli <niharikakohli29@gmail.com>
2016-04-02 17:16:53 +0300

    Attempt to fetch wikiprojects - 1

--- public_html/index.php
+++ public_html/index.php
@@ -15,9 +15,11 @@ $html .= '<tr>
 			<th>Diff</th>
 			<th>Date</th>
 		</tr>';
+
 if ( $result->num_rows > 0 ) {
 	while ( $row = $result->fetch_assoc() ) {
 		$wiki = "https://" . $row['lang'] . '.' . $row['project'] . '.org';
+		$wikiprojects = getWikiprojects( $wiki, $row['page_title'] );
 		$html .= '<tr class="trow">'
 					.'<td>'. $wiki . '</td>'
 					.'<td>'. $row['page_title'] . '</td>'
@@ -25,6 +27,18 @@ if ( $result->num_rows > 0 ) {
 					.'<td>'. $row['diff_timestamp'] .'</td>'
 				.'</tr>';
 	}
+}
+
+function getWikiprojects( $wiki, $page ) {
+	$curl = curl_init();
+	$url = $wiki . '/w/api.php?action=query&titles=Talk:' . $page . 'prop=templates&tllimit=max&formatversion=2';
+	curl_setopt( $curl, CURLOPT_PUT, 1 );
+	curl_setopt( $curl, CURLOPT_URL, $url );
+	curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
+	$result = curl_exec( $curl );
+	curl_close( $curl );
+	echo $result;
+    return $result;
 }
 
 echo $html;
