bc08e7626d3ab44d13673512963388155841d898
Niharika Kohli <niharikakohli29@gmail.com>
2016-04-02 15:01:24 +0300

    Fix credentials

--- public_html/index.php
+++ public_html/index.php
@@ -2,11 +2,11 @@
 
 $db = parse_ini_file( '/../replica.my.cnf' );
 
-$link = mysqli_connect( 'enwiki.labsdb', $db['user'], $db['password'], 's51306__copyright_p' );
+$link = mysqli_connect( 'enwiki.labsdb', $db['user'], $db['password'], 's51396__copyright_p' );
 
 echo 'Hello, I\'m plagiabot!';
 
-$query = "SELECT id, project, page_title, report FROM copyright_diffs LIMIT 10";
+$query = "SELECT * FROM copyright_diffs LIMIT 10";
 
 echo $query;
 
