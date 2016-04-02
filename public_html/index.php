3faf879195e83bfc698e8a77410e1dd224d2233d
Niharika Kohli <niharikakohli29@gmail.com>
2016-04-02 15:33:03 +0300

    Baby tables

--- public_html/index.php
+++ public_html/index.php
@@ -1,24 +1,29 @@
 <?php
 
-$db = parse_ini_file( '/../replica.my.cnf' );
+$db = parse_ini_file( '../replica.my.cnf' );
 
-$link = mysqli_connect( 'enwiki.labsdb', $db['user'], $db['password'], 's51396__copyright_p' );
-
-echo $db['user'];
+$link = mysqli_connect( 'enwiki.labsdb', $db['user'], $db['password'], 's51306__copyright_p' );
 
 $query = "SELECT * FROM copyright_diffs LIMIT 10";
 
-echo $query;
-
 $result = mysqli_query( $link, $query );
 
-var_dump( $result );
-
+$html = '<table>';
+$html .= '<tr>
+			<th>Wiki</th>
+			<th>Diff</th>
+			<th>Date</th>
+		</tr>';
 if ( $result->num_rows > 0 ) {
 	while ( $row = $result->fetch_assoc() ) {
-		echo $row['id'], $row['project'], $row['page_title'], $row['report'];
+		$wiki = "https://" + $row['lang'] + '.' + $row['project'] + '.org';
+		$html .= '<tr class="trow">'
+					.'<td>'. $wiki . '</td>'
+					.'<td><a href="'. $wiki . '?diff=prev&oldid='. $row['diff'].'">'. $row['diff'] .'</td>'
+					.'<td>'. $row['diff_timestamp'] .'</td>'
+				.'</tr>';
 	}
 }
 
-
+echo $html;
 ?>
