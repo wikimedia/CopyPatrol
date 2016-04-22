67813582eade8d5cf1a0b242524da4eaae5bf06b
Niharika Kohli <niharikakohli29@gmail.com>
2016-04-22 09:35:22 +0530

    Add header and tabs

--- public_html/templates/header.php
+++ public_html/templates/header.php
@@ -10,10 +10,22 @@
 <body>
 <div id="header">
 	<h1>Plagiabot</h1>
-	<ul class="nav global">
-		<li><a href="#">Unchecked</a></li>
-		<li><a href="#">Recent changes</a></li>
-		<li><a href="#">True positives</a></li>
+	<ul class="nav global nav-pills nav-justified">
+		<li><a data-toggle="pill" href="#tab1">Unchecked</a></li>
+		<li><a data-toggle="pill" href="#tab2">Recent changes</a></li>
+		<li><a data-toggle="pill" href="#tab3">True positives</a></li>
 	</ul>
 
+	<div class="tab-content">
+		<div id="tab1" class="tab-pane fade in active">
+		</div>
+		<div id="tab2" class="tab-pane fade">
+			<h3>Menu 1</h3>
+			<p>Some content in menu 1.</p>
+		</div>
+		<div id="tab3" class="tab-pane fade">
+			<h3>Menu 2</h3>
+			<p>Some content in menu 2.</p>
+		</div>
+	</div>
 </div>
