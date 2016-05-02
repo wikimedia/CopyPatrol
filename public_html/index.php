a4515f7dd4fe0850a57c5c521c796ab3acac82f5
Niharika Kohli <niharikakohli29@gmail.com>
2016-05-02 21:39:54 +0530

    Try adding collapsible div

--- public_html/index.php
+++ public_html/index.php
@@ -15,7 +15,9 @@ foreach ( $data as $k => $d ) {
 		. '<td class="col-md-1 text-center"><a href="' . $d['diff'] . '" target="_blank">Diff</a></td>'
 		. '<td class="col-md-1 text-center">' . $d['timestamp'] . '</td>'
 		. '<td class="col-md-4"><a href="' . $d['page_link'] . '" target="_blank">' . $d['page'] . '</a></td>'
-		. '<td class="col-md-2 text-center"><a href="' . $d['turnitin_report'] . '" target="_blank">Report</a></td>'
+		//. '<td class="col-md-2 text-center"><a href="' . $d['turnitin_report'] . '" target="_blank">Report</a></td>'
+		. '<td><button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Report</button>'
+		. '<div id="demo" class="collapse"><a href="' . $d['turnitin_report'] . '" target="_blank">Report link</a></div></td>'
 		. '<td class="col-md-3 text-center">';
 	foreach ( $d['wikiprojects'] as $w ) {
 		$html .= '<div class="wproject">' . $w . '</div>';
@@ -23,6 +25,13 @@ foreach ( $data as $k => $d ) {
 	$html .= '</td></tr>';
 }
 ?>
+
+
+<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Report</button>
+<div id="demo" class="collapse">
+	<a href="' . $d['turnitin_report'] . '" target="_blank">Report link</a>
+</div>
+
 
 <!--@foff-->
 <html>
