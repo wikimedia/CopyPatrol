872b92bca223604e4e93b34e4f33860a86ba12af
Niharika Kohli <niharikakohli29@gmail.com>
2016-05-02 22:08:31 +0530

    Buttons

--- public_html/index.php
+++ public_html/index.php
@@ -7,29 +7,30 @@ $data = $plagiabot->run();
 $html = '<tr class="container-fluid">
 			<th class="col-md-1 text-center">Diff</th>
 			<th class="col-md-1 text-center">Timestamp</th>
-			<th class="col-md-4 text-center">Page</th>
+			<th class="col-md-2 text-center">Page</th>
 			<th class="col-md-2 text-center">Turnitin Report</th>
-			<th class="col-md-3 text-center">Wikiprojects</th>';
+			<th class="col-md-3 text-center">Wikiprojects</th>
+			<th class="col-md-2" text-center">Review</th>
+		</tr>';
 foreach ( $data as $k => $d ) {
 	$html .= '<tr class="container-fluid">'
 		. '<td class="col-md-1 text-center"><a href="' . $d['diff'] . '" target="_blank">Diff</a></td>'
 		. '<td class="col-md-1 text-center">' . $d['timestamp'] . '</td>'
-		. '<td class="col-md-4"><a href="' . $d['page_link'] . '" target="_blank">' . $d['page'] . '</a></td>'
+		. '<td class="col-md-2"><a href="' . $d['page_link'] . '" target="_blank">' . $d['page'] . '</a></td>'
 		. '<td class="col-md-2 text-center"><a href="' . $d['turnitin_report'] . '" target="_blank">Report</a></td>'
 		. '<td class="col-md-3 text-center">';
 	foreach ( $d['wikiprojects'] as $w ) {
 		$html .= '<div class="wproject">' . $w . '</div>';
 	}
-	$html .= '</td></tr>';
+	$html .= '</td>';
+	$html .= '<td class="col-md-2 text-center">
+				<button type="button" class="btn btn-success" title=" The edit was a copyright violation and has been reverted">Page fixed</button>
+				<button type="button" class="btn btn-warning" title="The edit looks like copyright violation. Requesting someone else to take a look.">Request second opinion</button>
+				<button type="button" class="btn btn-danger" title="The edit is a false positive, nothing needs to be done">No action needed</button>
+			</td>';
+	$html .= '</tr>';
 }
 ?>
-
-
-<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Report</button>
-<div id="demo" class="collapse">
-	<a href="' . $d['turnitin_report'] . '" target="_blank">Report link</a>
-</div>
-
 
 <!--@foff-->
 <html>
