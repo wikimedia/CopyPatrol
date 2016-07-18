74419d47a53d688012b37f5225fefcf3fbb799c6
Kaldari <rkaldari@wikimedia.org>
2016-07-18 10:07:37 -0700

    Oops, static method

--- src/Controllers/CopyPatrol.php
+++ src/Controllers/CopyPatrol.php
@@ -160,7 +160,7 @@ class CopyPatrol extends Controller {
 		$deadPages = $asyncResults['deadPages'];
 
 		// Get ORES scores for edits
-		$oresScores = $this->oresScores( $diffIds );
+		$oresScores = self::oresScores( $diffIds );
 
 		// now all external requests and database queries (except
 		// WikiProjects) have been completed, let's loop through the records
